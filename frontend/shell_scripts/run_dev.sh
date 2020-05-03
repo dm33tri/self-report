92bf840665d08c4337dca83bf3d0987d8752c665
Aleks <veselev.aleks@gmail.com>
2020-05-03 11:15:59 +0300

    front docker

---
+++ frontend/shell_scripts/run_dev.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$ENV" = "dev" ]; then
+    if [ "package.json" -nt ".package.json" ]; then
+	echo "Running npm install"
+	npm install
+	touch .package.json
+    fi
+  echo "Starting dev server"
+  exec npm start
+fi
