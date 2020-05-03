d3a85753fce240d1978f1a17073ddb2ff53599ab
Aleks <veselev.aleks@gmail.com>
2020-05-03 13:06:45 +0300

    allow all

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'None')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'backend']
+ALLOWED_HOSTS = ['0.0.0.0', 'backend', '*']
 
 
 # Application definition
